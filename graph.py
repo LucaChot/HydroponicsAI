dbbe92f7a26cf1e06781fed3e7fe85ee8a207194
LucaChot <44319403+LucaChot@users.noreply.github.com>
2019-08-28 12:34:57 +0100

    update

--- graph.py
+++
@@ -1,7 +0,0 @@
-#This was me messing around with graphs to see if the equation given for light matched the given graph
-import matplotlib.pyplot as plt 
- 
-plt.figure()
-plt.subplot(1,1,1)
-plt.plot([1,2,3,4])
-plt.show
--- simulation.py
+++ simulation.py
@@ -1,15 +1,12 @@
 import random
-import matplotlib.pyplot as plt 
-
-def graph():
+import math
 
 #function to calculate growth depending on light
-def light_growth():
+def light_growth(I):
     p = 0
     a = 0.004547 #variables declared by paper on activity of chloroplasts depending on light
     b = 0.9901
     c = 4.064
-    I = random.randint(0,100)
     print(I)
     #this was the equation of cholorplast activity to light intensity
     p = I/(a*(I^2) + b*I + c)
@@ -24,4 +21,31 @@ def convec_calc():
     A = int(input("Enter surface area within plant"))
     q = hc*A*dT
     print(q)
+
+def temp_growth(T):
+    R = math.sqrt(0.89)
+    T_max = 37.1
+    T_opt = 24.6
+    R_max = 0.19
+    R  = R_max * ((T_max - T)/(T_max - T_opt)) * (T/T_opt)
+    print(R,T)
+
+
+def start_grow():
+    height = 0
+    temp = 20
+    light = 0
+
+    return height, temp, light
+
+
+        
+
+def main():
+    for i in range (0,35):
+        temp_growth(i)
+
+
+main()
+
 
